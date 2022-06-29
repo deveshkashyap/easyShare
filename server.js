939051268c3482723a776879e200486e28bddbae
dev <deveshkashyap48@gmail.com>
2022-06-29 17:14:41 +0530

    add cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
@@ -10,6 +10,13 @@ app.use(express.json());
 const connectDB = require('./config/db');
 // C:\Users\DEVESH\Desktop\file-sharing\config\db.js
 connectDB();
+
+//cors
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENS.split(',')
+}
+
+app.use(cors(corsOptions));
 //Template engine
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
