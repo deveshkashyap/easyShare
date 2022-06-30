eacb7ae27c0cccf3e1531ff5ed874733ab5c8c95
dev <deveshkashyap48@gmail.com>
2022-06-30 19:57:46 +0530

    sc

---
+++ scripts.js
@@ -0,0 +1,27 @@
+
+const File = require('./models/file');
+const fs = require('fs');
+const connectDB = require('./config/db');
+
+async function fetchData(){
+    // 24 hours
+    const pastDate = new Date(Date.now() - 24 * 60 * 60 * 1000);
+    const files = await File.find({ createdAt: {$lt: pastDate}})
+    if(files.length){
+        for(const file of files){
+            try{
+                fs.unlinkSync(file.path);
+                await file.remove();
+                console.log(`successfully deleted ${file.filename}`);
+            } catch(err){
+                console.log(`Error while deleting file ${err}`);
+            }
+        }
+        console.log('Job done');
+
+    }
+}
+
+fetchData().then(process.exit());
+    // console.log('Job done');
+    
