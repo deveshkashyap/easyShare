4cfc368b5531a0da7a797566b4132577e79b207d
dev <deveshkashyap48@gmail.com>
2022-06-30 15:28:01 +0530

    port change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 
-const PORT = (3000);
+const PORT = (process.env.PORT || 3000);
 app.use(express.static('public'));
 app.use(express.json());
 
